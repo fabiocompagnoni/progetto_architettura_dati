e472239975d2520e25d13b9ea99454ef6816ae2f
Fabio Compagnoni <fabiocompagnoni5@protonmail.com>
2026-07-04 11:26:59 +0200

    pool di nomi e cognomi italiani

---
+++ data/nomi.py
@@ -0,0 +1,58 @@
+"""Pool di nomi e cognomi italiani comuni (dato pubblico). Nome e cognome sono tenuti in
+liste separate e ricombinati a caso: nessuna persona reale viene riprodotta."""
+
+NOMI_M = [
+    "Marco", "Andrea", "Luca", "Alessandro", "Francesco", "Matteo", "Lorenzo", "Davide",
+    "Simone", "Federico", "Giovanni", "Antonio", "Giuseppe", "Stefano", "Roberto", "Paolo",
+    "Riccardo", "Michele", "Daniele", "Gabriele", "Nicola", "Emanuele", "Fabio", "Alberto",
+    "Giacomo", "Filippo", "Vincenzo", "Salvatore", "Angelo", "Domenico", "Pietro", "Enrico",
+    "Claudio", "Mauro", "Massimo", "Sergio", "Franco", "Giorgio", "Luigi", "Carlo", "Bruno",
+    "Aldo", "Dario", "Cristian", "Manuel", "Samuele", "Tommaso", "Edoardo", "Leonardo",
+    "Mattia", "Diego", "Alessio", "Christian", "Ivan", "Gianluca", "Raffaele", "Rocco",
+    "Cosimo", "Gennaro", "Ciro", "Umberto", "Renato", "Fabrizio", "Maurizio", "Walter",
+    "Marcello", "Elia", "Nicolo", "Valerio", "Gaetano", "Alfredo", "Guido", "Ettore",
+    "Fernando", "Emilio", "Vittorio", "Giulio", "Alessandro", "Michelangelo", "Cesare",
+    "Ferdinando", "Corrado", "Gustavo", "Loris", "Denis", "Kevin", "Thomas", "Jacopo",
+    "Federico", "Pierluigi", "Gianmarco", "Alan",
+]
+
+NOMI_F = [
+    "Giulia", "Sara", "Chiara", "Francesca", "Martina", "Alessia", "Anna", "Giorgia",
+    "Valentina", "Elena", "Federica", "Silvia", "Laura", "Alice", "Sofia", "Aurora",
+    "Greta", "Beatrice", "Elisa", "Ilaria", "Marta", "Camilla", "Gaia", "Noemi", "Arianna",
+    "Roberta", "Claudia", "Cristina", "Paola", "Simona", "Monica", "Barbara", "Antonella",
+    "Maria", "Rosa", "Angela", "Giovanna", "Teresa", "Lucia", "Carla", "Daniela", "Rita",
+    "Caterina", "Michela", "Serena", "Debora", "Veronica", "Eleonora", "Letizia", "Irene",
+    "Jessica", "Vanessa", "Nadia", "Sabrina", "Stefania", "Emanuela", "Manuela", "Patrizia",
+    "Raffaella", "Valeria", "Alessandra", "Elisabetta", "Giada", "Ludovica", "Matilde",
+    "Bianca", "Emma", "Vittoria", "Rebecca", "Nicole", "Cristiana", "Loredana", "Gabriella",
+    "Enrica", "Ornella", "Marika", "Katia", "Ambra", "Deborah", "Fabiana", "Rossana",
+    "Annalisa", "Miriam", "Erika", "Viola", "Melissa", "Aurelia", "Concetta", "Assunta",
+    "Filomena", "Carmela",
+]
+
+COGNOMI = [
+    "Rossi", "Russo", "Ferrari", "Esposito", "Bianchi", "Romano", "Colombo", "Ricci",
+    "Marino", "Greco", "Bruno", "Gallo", "Conti", "De Luca", "Mancini", "Costa", "Giordano",
+    "Rizzo", "Lombardi", "Moretti", "Barbieri", "Fontana", "Santoro", "Mariani", "Rinaldi",
+    "Caruso", "Ferrara", "Galli", "Martini", "Leone", "Longo", "Gentile", "Martinelli",
+    "Vitale", "Lombardo", "Serra", "Coppola", "De Santis", "D'Angelo", "Marchetti", "Parisi",
+    "Villa", "Conte", "Ferretti", "Bianco", "Fabbri", "Marini", "Grasso", "Valentini",
+    "Messina", "Sala", "De Angelis", "Gatti", "Pellegrini", "Palumbo", "Sanna", "Farina",
+    "Rizzi", "Monti", "Cattaneo", "Morelli", "Amato", "Silvestri", "Mazza", "Testa",
+    "Grassi", "Pellegrino", "Carbone", "Giuliani", "Benedetti", "Barone", "Rossetti",
+    "Caputo", "Montanari", "Guerra", "Palmieri", "Bernardi", "Martino", "Fiore", "De Rosa",
+    "Ferro", "Basile", "Riva", "Donati", "Piras", "Sartori", "Neri", "Costantini", "Milani",
+    "Ferraro", "Bellini", "Fumagalli", "Battaglia", "Simonetti", "Pagano", "Rossini",
+    "Serafini", "Riccardi", "Orlando", "Damico", "Negri", "Cavallaro", "Piccolo", "Ruggiero",
+    "Vitali", "Bevilacqua", "Testi", "Marchesi", "Franceschini", "Angelini", "Ferri",
+    "Pace", "Pinna", "Colella", "Giannini", "Salerno", "Bruni", "Nardini", "Fiorini",
+    "Rossetto", "Lorenzi", "Perrone", "Mele", "Guidi", "Melis", "Sorrentino", "Aiello",
+    "Landi", "Villani", "Ferrero", "Molinari", "Gagliardi", "Cocco", "Marra", "Poli",
+    "Carraro", "Zanetti", "Rota", "Bassi", "Beretta", "Corti", "Colombi", "Airoldi",
+    "Sangalli", "Manzoni", "Redaelli", "Brambilla", "Corradini", "Ferrari", "Locatelli",
+    "Invernizzi", "Bonacina", "Cazzaniga", "Perego", "Rigamonti", "Valsecchi", "Mauri",
+    "Fumagalli", "Crippa", "Galbiati", "Panzeri", "Spinelli", "Riva", "Radaelli",
+    "Bonfanti", "Corti", "Vergani", "Frigerio", "Anghileri", "Bonacina", "Dell'Oro",
+    "Maggioni", "Villa", "Ripamonti", "Casati", "Consonni", "Fontana", "Pozzi",
+]
